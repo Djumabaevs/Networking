4ed7a7fec4cf642b19181763b88489ba58463275
Djumabaevs <djumabaevb@gmail.com>
2021-03-20 18:44:20 +0600

    networking2

---
+++ src/com/company/Connection3.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+public class Connection3 {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://example.org");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent" ,"Chrome");
+            connection.setReadTimeout(30000);
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code " + responseCode);
+
+            if(responseCode != 200) {
+                System.out.println("Error reading web page");
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            while ((line = inputReader.readLine()) != null){
+                System.out.println(line);
+            }
+            inputReader.close();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
